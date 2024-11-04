34b7b20f69ec962fdd726ea84063f4b27e595d3e
Rishmo <sweetmoonsahu@gmail.com>
2024-11-04 19:43:02 +0530

    file added

---
+++ ExpressDir/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+// express is actually a function which we will execute
+const app = express();
+// by convention we give the name app and it helps us to create server side app.
+// app is acutally an object
+
+console.dir(app);
---
+++ Figletdir/index.js
@@ -0,0 +1,10 @@
+const figlet= require("figlet");
+
+figlet("Moon", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
