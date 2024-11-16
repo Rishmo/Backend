4779b044a15e5324a48d112a88c853dead179289
Rishmo <sweetmoonsahu@gmail.com>
2024-11-16 18:15:01 +0530

    file added

--- REST_API/index.js
+++ REST_API/index.js
@@ -8,6 +8,10 @@ const port= 8080; // for our local server
 // requiring uuid for creating unique id for post
 const { v4: uuidv4 } = require('uuid');
 
+//requiring method-override
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
 
 // serving static files
 
@@ -79,11 +83,31 @@ app.get("/posts/:id", (req, res) => { // to get only one post
 
 
 app.patch("/posts/:id", (req, res) => { 
-    let {id} = req.params;
+    let { id } = req.params;
     let newContent = req.body.content;
-    console.log(newContent);
+
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent; // resetting to newContent
+    console.log(post);
+    // console.log(newContent);
     res.send("patch request working");
+    res.redirect("/posts");
 });
+
+
+//for edit
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
+
+});
+
+app.delete("/posts/:id", (req, res) =>{
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+})
 
 
 app.listen(port, () => {
